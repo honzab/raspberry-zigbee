4797cdbb4b10ef01ed8224926dbf01bb2c5c75e8
Jan Brucek <jan@brucek.se>
2025-04-07 11:47:30 +0200

    Working version of inkywhat

--- inkywhat/inkywhat.go
+++ inkywhat/inkywhat.go
@@ -6,7 +6,6 @@ import (
 	"image/png"
 	"log"
 	"os"
-
 	"periph.io/x/conn/v3/gpio/gpioreg"
 	"periph.io/x/conn/v3/i2c/i2creg"
 	"periph.io/x/conn/v3/spi/spireg"
@@ -55,15 +54,14 @@ func initialiseInkyWhat() *inky.Dev {
 }
 
 func ClearDisplay() {
-	dev := initialiseInkyWhat()
-
-	if err := dev.Draw(img.Bounds(), img, image.Point{}); err != nil {
-		log.Fatal(err)
-	}
-
+	//dev := initialiseInkyWhat()
+	//
+	//if err := dev.Draw(image.Rectangle{image.Point{0, 0}, image.Point{400, 300}}, img, image.Point{}); err != nil {
+	//	log.Fatal(err)
+	//}
 }
 
-func SetImageOnInky(string path) {
+func SetImageOnInky(path *string) {
 
 	f, err := os.Open(*path)
 	if err != nil {
@@ -86,10 +84,84 @@ func SetImageOnInky(string path) {
 	}
 }
 
+// randomPoints returns some random x, y points.
+//func randomPoints(n int) plotter.XYs {
+//	pts := make(plotter.XYs, n)
+//	for i := range pts {
+//		if i == 0 {
+//			pts[i].X = rand.Float64()
+//		} else {
+//			pts[i].X = pts[i-1].X + rand.Float64()
+//		}
+//		pts[i].Y = pts[i].X + 10*rand.Float64()
+//	}
+//	return pts
+//}
+
+//func genImage() {
+//	p := plot.New()
+//
+//	p.Title.Text = "Plotutil example"
+//	p.X.Label.Text = "X"
+//	p.Y.Label.Text = "Y"
+//
+//	err := plotutil.AddLinePoints(p,
+//		"First", randomPoints(15),
+//		"Second", randomPoints(15),
+//		"Third", randomPoints(15))
+//	if err != nil {
+//		panic(err)
+//	}
+//
+//	// Save the plot to a PNG file.
+//	//if err := p.Save(vg.Points(300), vg.Points(225), "points.png"); err != nil {
+//	//	panic(err)
+//	//}
+//	if err := betterSave(p, vg.Points(300), vg.Points(225), "points.png"); err != nil {
+//		panic(err)
+//	}
+//
+//	time.Sleep(2 * time.Second)
+//	log.Printf("Saved plot to points.png")
+//
+//}
+
+//func betterSave(p *plot.Plot, w vg.Length, h vg.Length, file string) error {
+//	f, err := os.Create(file)
+//	if err != nil {
+//		return err
+//	}
+//	defer func() {
+//		e := f.Close()
+//		if err == nil {
+//			err = e
+//		}
+//	}()
+//
+//	format := strings.ToLower(filepath.Ext(file))
+//	if len(format) != 0 {
+//		format = format[1:]
+//	}
+//	c, err := p.WriterTo(w, h, format)
+//	if err != nil {
+//		return err
+//	}
+//
+//	_, err = c.WriteTo(f)
+//	f.Sync()
+//	return err
+//}
+
+func ptr(s string) *string { return &s }
+
 func main() {
 	path := flag.String("image", "", "Path to image file (400x300) to display")
 	flag.Parse()
 
+	//path := strings.Clone("points.png")
+
+	//genImage()
+	//SetImageOnInky(ptr("prdoch.png"))
 	if path != nil {
 		SetImageOnInky(path)
 	} else {
